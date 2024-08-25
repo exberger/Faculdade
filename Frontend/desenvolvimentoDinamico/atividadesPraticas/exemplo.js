79acbc195352356b173175f724b1eb3ff4845723
exberger <ex.asaaf@proton.me>
2024-08-25 16:38:06 -0300

    Primeira Aula Desenvolvimento Dinamico

---
+++ Frontend/desenvolvimentoDinamico/atividadesPraticas/exemplo.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual seu nome? ");
+
+let idade = prompt("Qual a sua idade? ");
+
+alert("MEU NOME É: " + nome + " Minha Idade é: " + idade)
---
+++ Frontend/desenvolvimentoDinamico/javaScript/scipt1.js
@@ -0,0 +1,4 @@
+let nome = "Gabriel Asaaf"
+let idade = "25"
+
+console.log("Meu nome é: " + nome + " Minha idade é: " + idade )
